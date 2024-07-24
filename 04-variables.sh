64473a99e4a700166956bd19af6fc99f5bab9af5
aws-devops26 <srivalli26.najara@gmail.com>
2024-07-24 21:31:56 +0530

    2nd variables

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: hi $PERSON2,how r u"
+echo "$PERSON2: i m fine $PERSON1,how r u"
+echo "$PERSON1: fine $PERSON2"
+echo "$PERSON2: good $PERSON1"
