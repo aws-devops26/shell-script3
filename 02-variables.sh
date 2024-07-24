5d819a251cb41120dc70460bc54387169433b95c
aws-devops26 <srivalli26.najara@gmail.com>
2024-07-24 16:17:53 +0530

    2nd script about variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "sri: hi venkat,how r u"
-echo "venkat: i m fine sri,how r u"
-echo "sri: fine"
-echo "venkat: good"
+person1=sri
+person2=venkat
+echo "$person1: hi $person2,how r u"
+echo "$person2: i m fine $person1,how r u"
+echo "$person1: fine $person2"
+echo "$person2: good"
