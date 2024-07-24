e0e1a0203bff4c18416fa2c3277abe6fefcaf463
aws-devops26 <srivalli26.najara@gmail.com>
2024-07-24 21:50:16 +0530

    2nd variables

---
+++ 08-arrays.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+fruits=("apple" "mango" "grapes")
+echo " 1st value: ${fruits[0]}"
+echo " 2nd valuse is ${fruits[1]}"
+echo " 3nd valuse is ${fruits[2]}"
+echo " all fruits : ${fruits[@]}"
