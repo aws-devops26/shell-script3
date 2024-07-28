236ddd9fd5082a0bfbc995020c1a995b6cfff0a7
aws-devops26 <srivalli26.najara@gmail.com>
2024-07-28 16:54:06 +0530

    adding scripts

---
+++ 10-install-mysql.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ID = $(id -u)
+if [ $ID -ne 0 ]
+then 
+    echo "error:: please run with root access "
+    exit 1 # other than 0 we need to give 
+else
+    echo "ur a root user"
+fi
+yum install mysql -y
+if [ $? -ne 0 ]
+then
+    echo " error:: installing ,my sql failed"
+    exit 1
+else
+    echo "installing mysql success"
+fi
