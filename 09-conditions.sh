395851e1d930bce3eabdb2132a127697e65c8466
aws-devops26 <srivalli26.najara@gmail.com>
2024-07-28 16:46:21 +0530

    adding scripts

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 NUMBER=$1
-if[ $NUMBER -gt 100 ]
+if [ $NUMBER -gt 100 ]
 then
     echo " $NUMBER is greater than 100 "
-    else
+else
     echo " $NUMBER is not greater than 100 "
 fi
