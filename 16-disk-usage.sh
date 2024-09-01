07c16a388fa8265a57dd608f7209b5b27bc54328
aws-devops26 <srivalli26.najara@gmail.com>
2024-09-01 13:56:35 +0530

    disk usage alert

--- 16-disk-usage.sh
+++ 16-disk-usage.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 DISK_USAGE=$(df -hT | grep -vE 'tmp|file')
 DISK_THRESHOLD=1
-message=""
-
+message=" :"
 while IFS= read line
 do 
     usage=$(echo $line | awk '{print $6f}' | cut -d % -f1)
     partition=$(echo $line | awk '{print $1f}')
-    if [ $usage -gt $DISK_THRESHOLD ]
+    if [ $usage -ge $DISK_THRESHOLD ]
     then
         message+="HIGH DISK USAGE ON $partition: $usage\n"
     fi
