11f12047622ea448ac03677a68b2550555709b46
aws-devops26 <srivalli26.najara@gmail.com>
2024-09-01 14:48:46 +0530

    disk usage alert

--- 16-disk-usage.sh
+++ 16-disk-usage.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-DISK_USAGE=$(df -hT | grep -vE 'tmp|file')
+
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
 DISK_THRESHOLD=1
 message=""
 
 while IFS= read line
-do 
-    usage=$(echo $line | awk '{print $6f}' | cut -d % -f1)
-    partition=$(echo $line | awk '{print $1f}')
+do
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+    partition=$(echo $line | awk '{print $1F}')
     if [ $usage -ge $DISK_THRESHOLD ]
     then
-        message+="HIGH DISK USAGE ON $partition: $usage\n"
+        message+="High Disk Usage on $partition: $usage <br>"
     fi
 done <<< $DISK_USAGE
 
-echo -e "Message:$message"
+echo -e "Message: $message"
+
+#echo "$message" | mail -s "High Disk Usage" info@joindevops.com
+
+sh mail.sh "DevOps Team" "High Disk Usage" "$message" "info@joindevops.com" "ALERT High Disk Usage"
