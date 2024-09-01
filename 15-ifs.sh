384b3bab67e29005a201b6753c1b3c759d4142fe
aws-devops26 <srivalli26.najara@gmail.com>
2024-09-01 14:54:59 +0530

    disk usage alert

--- 15-ifs.sh
+++ 15-ifs.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-file=/etc/passwd
+file= /etc/passwd
+R="\e[31m"
+N="\e[0m"
+if [ ! -f $file]
+then
+    echo -e "$R source file is: $file does not exists. $N"
+fi
+while IFS =":" read -r username password user_id group_id user_fullname home_dir 
+do
+    echo " username: $username"
+    echo " user id: $user_id"
+done < $file
