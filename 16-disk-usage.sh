06fe4efb5ef7d48f7d8f6c6cf3ec47497c50dc11
aws-devops26 <srivalli26.najara@gmail.com>
2024-09-01 14:49:50 +0530

    disk usage alert

--- 16-disk-usage.sh
+++ 16-disk-usage.sh
@@ -10,7 +10,7 @@ do
     partition=$(echo $line | awk '{print $1F}')
     if [ $usage -ge $DISK_THRESHOLD ]
     then
-        message+="High Disk Usage on $partition: $usage <br>"
+        message+="High Disk Usage on $partition: $usage\n"
     fi
 done <<< $DISK_USAGE
 
@@ -18,4 +18,3 @@ echo -e "Message: $message"
 
 #echo "$message" | mail -s "High Disk Usage" info@joindevops.com
 
-sh mail.sh "DevOps Team" "High Disk Usage" "$message" "info@joindevops.com" "ALERT High Disk Usage"
